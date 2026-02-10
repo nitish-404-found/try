6fca40f0b0071c20e38cbd22fdcfca47091a226a
nitish-404-found <nitish90901221@gmail.com>
2026-02-11 01:36:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node Server will thsi work?? 🚀");
+  res.end("Hello from Node Server will thsi work?? last check 🚀");
 });
 
 server.listen(PORT, () => {
